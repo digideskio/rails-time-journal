29edacd4f71048caa71067dfa371f996fa6191b4
petechappell <peter@experiencebureau.com>
2013-12-06 16:45:35 +1000

    Use concatenated JS/CSS better in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Tj1::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  #config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -34,6 +34,8 @@ Tj1::Application.configure do
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
+
+  #config.assets.precompile = ['*.js', '*.css', '*.css.erb', '*.css.scss', '*.js.coffee']
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
